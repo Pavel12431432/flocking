f556a2275eb33b45ed9e7ffd339eda5fb91fbbb1
Pavel <49321976+Pavel12431432@users.noreply.github.com>
2020-10-06 17:39:04 +0300

    Add files via upload

--- boid.py
+++ boid.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Boid:
 	def __init__(self, pos, velocity, max_speed, sight_radius, align_factor, cohesion_factor, separation_factor):
 		self.pos = pos
@@ -13,51 +12,48 @@ class Boid:
 		self.cohesion_factor = cohesion_factor
 		self.separation_factor = separation_factor
 
+	# update position and velocity
 	def update(self, screen_width, screen_height):
 		self.pos = tuple(map(sum, zip(self.pos, self.velocity)))
 		self.pos = self.pos[0] % screen_width, self.pos[1] % screen_height
 		self.velocity = tuple(map(sum, zip(self.velocity, self.acceleration)))
 		self.velocity = norm(self.velocity, self.max_speed)
-		self.acceleration = 0, 0
 
-	def align(self, flock):
-		direction = 0, 0
+	# flocking mechanic
+	def flock(self, flock):
+		alignment = 0, 0
+		cohesion = 0, 0
+		separation = 0, 0
 		total = 0
 		for boid in flock:
-			if math.sqrt((self.pos[0] - boid.pos[0]) ** 2 + (self.pos[1] - boid.pos[1]) ** 2) <= self.sight_radius:
-				direction = tuple(map(sum, zip(direction, boid.velocity)))
+			# check if current boid is approximately within sight_radius
+			# using Manhattan distance to compute faster (almost twice as fast)
+			dist = abs(self.pos[0] - boid.pos[0]) + abs(self.pos[1] - boid.pos[1])
+			if dist <= self.sight_radius and dist:
+				# calculate alignment and cohesion
+				alignment = tuple(map(sum, zip(alignment, boid.velocity)))
+				cohesion = tuple(map(sum, zip(cohesion, boid.pos)))
+				# calculate the vector opposite the current boid
+				# with a magnitude inversely proportional to the distance
+				opposite_dir = (self.pos[0] - boid.pos[0]) / dist, (self.pos[1] - boid.pos[1]) / dist
+				separation = separation[0] + opposite_dir[0], separation[1] + opposite_dir[1]
 				total += 1
+		# check if a boid is within sight_radius
 		if total:
-			self.acceleration = tuple(map(lambda x: x / total * self.align_factor, direction))
-
-	def cohesion(self, flock):
-		direction = 0, 0
-		total = 0
-		for boid in flock:
-			if math.sqrt((self.pos[0] - boid.pos[0]) ** 2 + (self.pos[1] - boid.pos[1]) ** 2) <= self.sight_radius:
-				direction = tuple(map(sum, zip(direction, boid.pos)))
-				total += 1
-		if total:
-			direction = (direction[0] / total - self.pos[0]) * self.cohesion_factor, \
-						(direction[1] / total - self.pos[1]) * self.cohesion_factor
-			self.acceleration = tuple(map(sum, zip(self.acceleration, direction)))
-
-	def separation(self, flock):
-		direction = 0, 0
-		total = 0
-		for boid in flock:
-			dist = math.sqrt((self.pos[0] - boid.pos[0]) ** 2 + (self.pos[1] - boid.pos[1]) ** 2)
-			if dist <= self.sight_radius:
-				op_dir = -(boid.pos[0] - self.pos[0]) / max(dist, 0.01), -(boid.pos[1] - self.pos[1]) / max(dist, 0.01)
-				direction = direction[0] + op_dir[0], direction[1] + op_dir[1]
-				total += 1
-		if total:
-			self.acceleration = tuple(
-				map(lambda x: x[0] + x[1] * self.separation_factor, zip(self.acceleration, direction)))
+			# steer towards the average heading of local flockmates
+			alignment = tuple(map(lambda x: x / total * self.align_factor, alignment))
+			# steer to average position of local flockmates
+			cohesion = (cohesion[0] / total - self.pos[0]) * self.cohesion_factor, \
+					   (cohesion[1] / total - self.pos[1]) * self.cohesion_factor
+		# steer to avoid crowding local flockmates
+		separation = tuple(map(lambda x: x * self.separation_factor, separation))
+		# update the acceleration
+		self.acceleration = tuple(map(sum, zip(alignment, cohesion, separation)))
 
 
-def norm(a, div):
-	v = math.sqrt(a[0] ** 2 + a[1] ** 2) / div
+# normalize a vector to a certain magnitude
+def norm(a, mag):
+	v = math.sqrt(a[0] ** 2 + a[1] ** 2) / mag
 	if v == 0:
 		return 0
 	return a[0] / v, a[1] / v
--- main.py
+++ main.py
@@ -1,52 +1,58 @@
+import math
+from random import randint, gauss
+
 import pygame
 from pygame import gfxdraw
-from random import randint, gauss
+
 from boid import Boid
 
-WIDTH, HEIGHT = 800, 600
+# define constants
+WIDTH, HEIGHT = 640, 480
 BOID_SPEED = 2
 BOID_SIGHT_RADIUS = 75
 ALIGN_FACTOR = 0.05
 COHESION_FACTOR = 0.01
 SEPARATION_FACTOR = 0.05
 
+# pygame init
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Flocking')
-font = pygame.font.SysFont('consolas', 15)
+font = pygame.font.SysFont('consolas', 30)
 clock = pygame.time.Clock()
 
 
+# handle input
 def inp():
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			quit()
 
 
+# draw boids
 def draw():
 	screen.fill((0, 0, 0))
 	for boid in flock:
-		gfxdraw.aacircle(screen, *tuple(map(int, boid.pos)), 2, (255, 255, 255))
+		gfxdraw.filled_circle(screen, *tuple(map(int, boid.pos)), 2, (255, 255, 255))
 	pygame.display.update()
 
 
-def make_rand_vector(dims):
-	vec = [gauss(0, 1) for i in range(dims)]
-	mag = sum(x ** 2 for x in vec) ** .5
+# generate random initial velocity for boids
+def make_rand_vector():
+	vec = gauss(0, 1), gauss(0, 1)
+	mag = math.sqrt(sum(x ** 2 for x in vec))
 	return [x / mag for x in vec]
 
 
-# noinspection ProblematicWhitespace
-flock = [Boid((randint(0, WIDTH), randint(0, HEIGHT)), make_rand_vector(2), BOID_SPEED, BOID_SIGHT_RADIUS, ALIGN_FACTOR,
-			  COHESION_FACTOR, SEPARATION_FACTOR) for i in range(100)]
+# generate the flock of boids
+flock = [Boid((randint(0, WIDTH), randint(0, HEIGHT)), make_rand_vector(), BOID_SPEED, BOID_SIGHT_RADIUS, ALIGN_FACTOR,
+			  COHESION_FACTOR, SEPARATION_FACTOR) for i in range(200)]
 
 while True:
-	draw()
-	inp()
 	flock_copy = flock
 	for boid in flock:
-		boid.align(flock_copy)
-		boid.cohesion(flock_copy)
-		boid.separation(flock_copy)
+		boid.flock(flock_copy)
 		boid.update(WIDTH, HEIGHT)
-	clock.tick(60)
+	inp()
+	draw()
+	clock.tick(0)
